a917d46ee71dcd862556ee6c9387cb2649de7a9f
vishwajeethaldar <onlyjeet3@gmail.com>
2022-07-19 23:49:42 +0530

    day-2

---
+++ src/price.js
@@ -0,0 +1,12 @@
+
+let changePlans = ()=>{
+    let plan  = document.getElementById("plans");
+    let market = document.getElementById("marketing");
+    let teams  = document.getElementById("teams");
+
+    let plans = [plan, market, teams];
+
+    plans.forEach((el)=>{
+        
+    });
+}
