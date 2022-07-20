2c46af2c36afedc7f4b4f70856481ca5a70bee61
vishwajeethaldar <onlyjeet3@gmail.com>
2022-07-21 00:17:55 +0530

    day 2 done

---
+++ components/priceNote.js
@@ -0,0 +1,11 @@
+let priceNote = ()=>{
+    let note = 
+    `
+    <p>
+        The pricing outlined herein is valid only as to new purchases occurring on or after July 1, 2021, and is not offered and does not apply to any prior purchases. Wrike reserves the right to adjust its packaging and pricing at any time.
+    </p>
+    `
+    return note;
+}
+
+export default priceNote;
