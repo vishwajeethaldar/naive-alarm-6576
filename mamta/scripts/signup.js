698e130bac288777466bb532e044008ea2bef328
mamtaashu <mamtaashu926@gmail.com>
2022-07-22 09:43:31 +0530

    done

--- mamta/scripts/signup.js
+++ mamta/scripts/signup.js
@@ -1,7 +1,28 @@
 document.querySelector("#btn").addEventListener("click",myfunction)
 
-
+var arr=JSON.parse(localStorage.getItem("alldata")) || []
 function myfunction()
 {
-    window.location.href="home.html";
+
+    var obj={
+   name:document.querySelector("#name").value,
+   phone:document.querySelector("#phone").value,
+  email:document.querySelector("#email").value,
+  password:document.querySelector("#comp").value
+
+    }
+    arr.push(obj)
+    console.log(arr)
+    localStorage.setItem("alldata",JSON.stringify(arr));
+
+
+    if(document.querySelector("#name").value=="" || document.querySelector("#phone").value=="" ||
+     document.querySelector("#email").value=="" || document.querySelector("#compgit ").value=="")
+    {
+        alert("enter all details")
+    }
+    else{
+        alert("signup successfully")
+        window.location.href="login.html"
+    }
 }
